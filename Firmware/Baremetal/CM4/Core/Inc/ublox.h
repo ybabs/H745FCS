d227956093ac9ae4224186e0409c9ab5e10d2935
Daniel <mogun123@gmail.com>
2022-01-02 21:34:40 +0000

    Refactor GPS Driver

--- Firmware/Baremetal/CM4/Core/Inc/ublox.h
+++ Firmware/Baremetal/CM4/Core/Inc/ublox.h
@@ -1,69 +1,58 @@
-/*
- * ublox.h
- *
- *  Created on: 16 Aug 2021
- *      Author: Daniel Babatunde
- */
-
 #ifndef INC_UBLOX_H_
 #define INC_UBLOX_H_
 
 #include "string.h"
 
 #define UBLOX_GXGGA_OFF (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x24}
-#define UBLOX_GXGLL_OFF (const char[])  {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x01,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x2B}
-#define UBLOX_GXGSA_OFF (const char[]) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x02,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x32}
-#define UBLOX_GXGSV_OFF  (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x03,0x00,0x00,0x00,0x00,0x00,0x01,0x03,0x39}
+#define UBLOX_GXGLL_OFF (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x01,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x2B}
+#define UBLOX_GXGSA_OFF (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x02,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x32}
+#define UBLOX_GXGSV_OFF (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x03,0x00,0x00,0x00,0x00,0x00,0x01,0x03,0x39}
 #define UBLOX_GXRMC_OFF (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x04,0x00,0x00,0x00,0x00,0x00,0x01,0x04,0x40}
 #define UBLOX_GXVTG_OFF (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x05,0x00,0x00,0x00,0x00,0x00,0x01,0x05,0x47}
 #define UBLOX_10HZ_RATE (const char []) {0xB5,0x62,0x06,0x08,0x06,0x00,0x64,0x00,0x01,0x00,0x01,0x00,0x7A,0x12}
 #define UBLOX_NAVPVT_ON (const char []) {0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x07,0x00,0x01,0x00,0x00,0x00,0x00,0x18,0xE1}
-#define UBX_HEADER (const char []){ 0xB5, 0x62 }
+#define UBX_HEADER      (const char []) {0xB5, 0x62 }
 
-typedef struct NAV_PVT {
+typedef struct NAV_PVT
+{
   unsigned char cls;
   unsigned char id;
   unsigned char length;
-  unsigned long iTOW; // GPS Time of Week in ms
-
-  unsigned short year; // UTC Year
-  unsigned char month; // month in UTC
+  unsigned long iTOW;               // GPS Time of Week in ms
+  unsigned short year;             // UTC Year
+  unsigned char month;            // month in UTC
   unsigned char day;
   unsigned char hour;
   unsigned char min;
   unsigned char sec;
   unsigned char validflag;
-  unsigned long tAcc; // time accuracy estimate in  ns
-  long nano;  // ns fraction of second
-  unsigned char fixType; // GNSS fix type: {0: no Fix, 1 dead reckoning, 2, 2D-fix, 3:3D fix, 4: GNSS + dead reckoning, 5: time fix
+  unsigned long tAcc;                        // time accuracy estimate in  ns
+  long nano;                                // ns fraction of second
+  unsigned char fixType;                   // GNSS fix type: {0: no Fix, 1 dead reckoning, 2, 2D-fix, 3:3D fix, 4: GNSS + dead reckoning, 5: time fix
   unsigned char fixStatusflag;
   unsigned char extendedflag;
-  unsigned char numSV; // number of satellites used
+  unsigned char numSV;                   // number of satellites used
   long longitude;
   long latitude;
-  long height; // height above ellipsoid
-  long hMSL; // height above mean sea level in mm
-  unsigned long hAcc; // // horizontal accuracy estimate
-  unsigned long vAcc; // vertical accuracy estimate
-
+  long height;                         // height above ellipsoid
+  long hMSL;                          // height above mean sea level in mm
+  unsigned long hAcc;                // horizontal accuracy estimate
+  unsigned long vAcc;               // vertical accuracy estimate
   long velN;
   long velE;
   long velD;
-  long gSpeed; // Ground Speed
-  long headMot; // 2D head motion
-  unsigned long sAcc; // speed acuracy motion
-  unsigned long headingAcc; // heading accuracy estimate
-
+  long gSpeed;                    // Ground Speed
+  long headMot;                  // 2D head motion
+  unsigned long sAcc;           // speed accuracy motion
+  unsigned long headingAcc;    // heading accuracy estimate
   unsigned short pDOP;
   unsigned char flags3;
-
   unsigned char reserved1;
   unsigned char reserved2;
   unsigned char reserved3;
   unsigned char reserved4;
   unsigned char reserved5;
-
-  long headVeh; // heading Velocity
+  long headVeh;               // heading Velocity
   short magDec;
   short magAcc;
  }NAV_PVT;
@@ -78,28 +67,13 @@ typedef struct NAV_PVT {
   float vel_z;
   float gndSpeed;
   int num_satellites;
-
-//  NAV_PVT pvt;
-
  }GPSHandle;
 
-// void calcChecksum(GPSHandle* gps, unsigned char* ck);
-// GPSHandle processGPS(GPSHandle* gps); // change to HAL_StatusTypeDef
-// void ConfigGPS(void);
-
-
  void calcChecksum(unsigned char* ck);
- void processGPS(GPSHandle* gps); // change to HAL_StatusTypeDef
+ void processGPS(GPSHandle* gps);
  void ConfigGPS(void);
-
-
-
  GPSHandle gps;
  NAV_PVT pvt;
-//uint8_t data_byte;
-
-
-
 
 
 #endif /* INC_UBLOX_H_ */
--- Firmware/Baremetal/CM4/Core/Src/ublox.c
+++ Firmware/Baremetal/CM4/Core/Src/ublox.c
@@ -1,9 +1,3 @@
-/*
- * ublox.c
- *
- *  Created on: 16 Aug 2021
- *      Author: Daniel Babatunde
- */
 
 #include "ublox.h"
 #include "usart.h"
@@ -11,17 +5,11 @@
 uint8_t data_byte[1] = {0};
 const int data_size = sizeof(NAV_PVT);
 
-// void calcChecksum(GPSHandle* gps, unsigned char* ck)
-// {
-//     memset(ck, 0, 2);
-//     for (int i = 0; i < (int)sizeof(gps->pvt); i++)
-//      {
-//        ck[0] = ck[0] + ((unsigned char*)(&gps->pvt))[i];
-//        ck[1] = ck[1] + ck[0];
-//      }
-//
-// }
-
+/*
+ * @brief Calculates checksum of incoming data packet
+ * @param ck pointer to unsigned char array containing checksum packets
+ * @retval nothing
+ */
  void calcChecksum(unsigned char* ck)
  {
      memset(ck, 0, 2);
@@ -30,94 +18,60 @@ const int data_size = sizeof(NAV_PVT);
         ck[0] = ck[0] + ((unsigned char*)(&pvt))[i];
         ck[1] = ck[1] + ck[0];
       }
-
  }
 
-
+/*
+ * @brief passes GPS info to gps handle struct
+ * @param gps pointer to gps struct
+ * @retval nothing
+ */
  void processGPS(GPSHandle* gps)
  {
      gps->latitude = pvt.latitude * 1.0e-7;
      gps->longitude = pvt.longitude * 1.0e-7;
-     gps->altitude = pvt.hMSL / 1000.0f;  // use Mean sea level instead
+     gps->altitude = pvt.hMSL / 1000.0f;  // use Mean sea level
 
      // format to ENU
      gps->vel_x = pvt.velE / 1000.0f;
      gps->vel_y = pvt.velN / 1000.0f;
      gps->vel_z = pvt.velD / -1000.0f;
      gps->gndSpeed = pvt.gSpeed / 1000.0f;
+     /* Retrieve number of satellites used */
      gps->num_satellites = pvt.numSV;
-
-
  }
 
-
-
-// GPSHandle processGPS(GPSHandle* gps)
-// {
-//     //GPSHandle gpsStruct;
-//
-//     gps->latitude =  gps->pvt.latitude * 1.0e-7;
-//     gps->longitude = gps->pvt.longitude * 1.0e-7;
-//     gps->altitude =  gps->pvt.height / 1000.0f;
-//
-//     // format to ENU
-//     gps->vel_x = gps->pvt.velE / 1000.0f;
-//     gps->vel_y = gps->pvt.velN / 1000.0f;
-//     gps->vel_z = gps->pvt.velD / -1000.0f;
-//     gps->gndSpeed = gps->pvt.gSpeed / 1000.0f;
-//
-//     return *gps;
-// }
-
-
-// GPSHandle processGPS(GPSHandle* gps)
-// {
-//     //GPSHandle gpsStruct;
-//
-//     gps->latitude =  gps->pvt.latitude * 1.0e-7;
-//     gps->longitude = gps->pvt.longitude * 1.0e-7;
-//     gps->altitude =  gps->pvt.height / 1000.0f;
-//
-//     // format to ENU
-//     gps->vel_x = gps->pvt.velE / 1000.0f;
-//     gps->vel_y = gps->pvt.velN / 1000.0f;
-//     gps->vel_z = gps->pvt.velD / -1000.0f;
-//     gps->gndSpeed = gps->pvt.gSpeed / 1000.0f;
-//
-//     return *gps;
-// }
-
-
+ /*
+  * @brief Configures GPS messages to be enabled on startup
+  * @retval nothing.
+  */
  void ConfigGPS(void)
  {
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_10HZ_RATE, sizeof(UBLOX_10HZ_RATE));
-   HAL_Delay(50);
+   HAL_Delay(20);
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_GXGGA_OFF, sizeof(UBLOX_GXGGA_OFF));
-   HAL_Delay(50);
+   HAL_Delay(20);
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_GXGLL_OFF, sizeof(UBLOX_GXGLL_OFF));
-   HAL_Delay(50);
+   HAL_Delay(20);
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_GXGSA_OFF, sizeof(UBLOX_GXGSA_OFF));
-   HAL_Delay(50);
+   HAL_Delay(20);
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_GXGSV_OFF, sizeof(UBLOX_GXGSV_OFF));
-   HAL_Delay(50);
+   HAL_Delay(20);
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_GXRMC_OFF, sizeof(UBLOX_GXRMC_OFF));
-   HAL_Delay(50);
+   HAL_Delay(20);
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_GXVTG_OFF, sizeof(UBLOX_GXVTG_OFF));
-   HAL_Delay(50);
+   HAL_Delay(20);
    HAL_UART_Transmit_DMA(&huart3, (uint8_t *)UBLOX_NAVPVT_ON, sizeof(UBLOX_NAVPVT_ON));
-
-   HAL_UART_Receive_DMA(&huart3, (uint8_t *)data_byte, 1);
- }
-
- void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
- {
-    if(huart->Instance == USART3)
-    {
-
-    }
+   // This is not needed
+   //HAL_UART_Receive_DMA(&huart3, (uint8_t *)data_byte, 1);
  }
 
 
+
+/*
+ * @brief Process GPS messages inside Callback
+ * @note Not sure of a better way to handle this at the moment
+ * @param huart Point to GPS UART Handle
+ */
  void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
  {
     static int byte_pos = 0;
@@ -125,6 +79,7 @@ const int data_size = sizeof(NAV_PVT);
 
     if(huart->Instance == USART3)
     {
+      /*Append Preamble*/
       if(byte_pos < 2)
       {
         if(data_byte[0] == UBX_HEADER[byte_pos])
@@ -149,7 +104,6 @@ const int data_size = sizeof(NAV_PVT);
         {
           calcChecksum(checksum);
         }
-
         else if (byte_pos == (data_size+3) )
         {
           if (data_byte[0] != checksum[0])
@@ -160,8 +114,6 @@ const int data_size = sizeof(NAV_PVT);
         else if (byte_pos == (data_size+4) )
         {
           byte_pos = 0;
-          //if( data_byte[0] == checksum[1] )
-            //LEDB_ON();
         }
         else if ( byte_pos > (data_size+4) )
         {
@@ -169,62 +121,14 @@ const int data_size = sizeof(NAV_PVT);
         }
       }
     }
-
-    //processGPS(&gps);
  }
 
-// void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-// {
-//    static int byte_pos = 0;
-//    static unsigned char checksum[2];
-//    GPSHandle gps;
-//
-//    if(huart->Instance == USART3)
-//    {
-//      if(byte_pos < 2)
-//      {
-//        if(data_byte[0] == UBX_HEADER[byte_pos])
-//        {
-//          byte_pos++;
-//        }
-//        else
-//        {
-//          byte_pos = 0;
-//        }
-//      }
-//
-//      else
-//      {
-//        if((byte_pos -2) < data_size)
-//        {
-//          ((unsigned char*)(&gps.pvt))[byte_pos-2] = data_byte[0];
-//        }
-//        byte_pos++;
-//
-//        if (byte_pos == (data_size+2))
-//        {
-//          calcChecksum(&gps, checksum);
-//        }
-//
-//        else if (byte_pos == (data_size+3) )
-//        {
-//          if (data_byte[0] != checksum[0])
-//          {
-//            byte_pos = 0;
-//          }
-//        }
-//        else if (byte_pos == (data_size+4) )
-//        {
-//          byte_pos = 0;
-//          //if( data_byte[0] == checksum[1] )
-//            //LEDB_ON();
-//        }
-//        else if ( byte_pos > (data_size+4) )
-//        {
-//          byte_pos = 0;
-//        }
-//      }
-//    }
-//
-//    //processGPS(&gps);
-// }
+
+ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+ {
+    if(huart->Instance == USART3)
+    {
+
+    }
+ }
+
